95bc4e10e9ea03a0d049720c95ad6c02c7d19903
pjp0704 <junhyeong25@gmail.com>
2021-08-17 20:04:34 +0900

    <Add> Custom logger middleware

--- nest-basic/src/app.module.ts
+++ nest-basic/src/app.module.ts
@@ -1,10 +1,16 @@
+import { MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
