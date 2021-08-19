dae0b06f6156972185ba63d99d71719603a188c5
pjp0704 <junhyeong25@gmail.com>
2021-08-19 17:03:39 +0900

    <Add> HTTP exception filter

--- nest-basic/src/app.controller.ts
+++ nest-basic/src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, UseFilters } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getHello(): string {
+    throw new HttpException('api broken', 401);
     return this.appService.getHello();
   }
 }
